4835ffe22cff46b2c6efe2c6cb0cc67e9e28f3d2
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-05-27 16:38:50 +0900

    login 작성

--- src/routes/Atuh.js
+++ src/routes/Atuh.js
@@ -1,4 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input,Button } from "@chakra-ui/react";
 
-const Auth = () => <div>Auth</div>;
+const Auth = () => {
+const [userInputValue, setUserInputValue] = useState({
+    email: '',
+    password: ''
+})
+const onChange = (e) => {
+    const {name, value} = e.target
+    setUserInputValue({ ...userInputValue, [name]:value})
+}
+
+console.log(userInputValue);
+
+return (
+    <div className="Auth">
+        <Input onChange={onChange} name='email' placeholder="email" />
+        <Input onChange={onChange} name="password" placeholder="password" />
+        <Button>Login</Button>
+        <Button>Create Account</Button>
+        <Button>Continue with Google</Button>
+        <Button>Continue with Github</Button>
+    </div>
+    )
+}
 export default Auth
