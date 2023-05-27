543defa178ac0784a68d618124984919975dbab9
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-05-27 16:03:22 +0900

    라이브러리 추가

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import AppRouter from "./Router";
+import {authService} from '../fbase'
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  console.log(isLoggedIn);
+
+  return (
+    <AppRouter isLoggedIn={isLoggedIn}/>
+  );
+}
+
+export default App;
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const EditProfile = () => <div>EditProfile</div>;
+export default EditProfile;
