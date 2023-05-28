cfccafeecf7c2d7c71ec6256dba362ae624a46a8
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-05-28 20:03:11 +0900

    Authentication 설정중

--- src/routes/Atuh.js
+++ src/routes/Atuh.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { Input,Button } from "@chakra-ui/react";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 const Auth = () => {
+const auth = getAuth();
 const [userInputValue, setUserInputValue] = useState({
     email: '',
     password: ''
 })
+const [newAccount ,setNewAccount] = useState(true)
 const onChange = (e) => {
     const {name, value} = e.target
     setUserInputValue({ ...userInputValue, [name]:value})
@@ -13,15 +20,33 @@ const onChange = (e) => {
 
 console.log(userInputValue);
 
+const submit = async(e) => {
+    try {
+        let data
+        if (newAccount) {
+            data = await createUserWithEmailAndPassword(auth, userInputValue.email, userInputValue.password)
+        } else {
+            data = await signInWithEmailAndPassword(auth, userInputValue.email, userInputValue.password)
+        }
+        console.log(data)
+        setUserInputValue({
+        email: '',
+        password: ''
+        });
+    } catch(error) {
+        console.log(error);
+    }
+  };
+
 return (
     <div className="Auth">
-        <Input onChange={onChange} name='email' placeholder="email" />
-        <Input onChange={onChange} name="password" placeholder="password" />
-        <Button>Login</Button>
-        <Button>Create Account</Button>
+        <Input onChange={onChange} name="email" value={userInputValue.email} placeholder="email" />
+        <Input onChange={onChange} name="password" value={userInputValue.password} placeholder="password" />
+        <Button onClick={submit}>{newAccount ? "Create Account" : "Login"}</Button>
         <Button>Continue with Google</Button>
         <Button>Continue with Github</Button>
     </div>
     )
 }
+
 export default Auth
